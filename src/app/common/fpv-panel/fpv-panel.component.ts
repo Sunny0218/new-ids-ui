447fe3045fe8b06ed50b8d1e16a0c19c2d280999
Sunny0218 <254450600@qq.com>
2021-12-17 17:58:11 +0800

    up to date 2021-12-17

--- src/app/common/fpv-panel/fpv-panel.component.ts
+++ src/app/common/fpv-panel/fpv-panel.component.ts
@@ -3,6 +3,9 @@ import { environment } from 'src/environments/environment';
 import { CommonPanelService } from '../common-panel.service';
 import { IMqttMessage, MqttService } from 'ngx-mqtt';
 import { Subscription } from 'rxjs';
+import Janus from '../../../assets/janus/janus.js';
+import adapter from 'webrtc-adapter'; 
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-fpv-panel',
@@ -18,6 +21,10 @@ export class FpvPanelComponent implements OnInit {
 
   batteryLevel:number = 0;
   isFlying: boolean = false;
+  showToolbar:boolean = false;
+
+  cctvStream = window.origin+"/janus/cctv.html";
+  safeCctvUrl:any
 
   //订阅获取无人机、遥控状态主题
   subTopic:string;
@@ -44,7 +51,7 @@ export class FpvPanelComponent implements OnInit {
       mode:'PIP'
     }];
 
-  //定义摄像模式默认参数
+  //定义镜头模式默认参数
   modeCommand: any = {
     "jsonrpc" : "2.0",
     "id" : "",
@@ -65,11 +72,14 @@ export class FpvPanelComponent implements OnInit {
 
   constructor( 
     private commonSvc: CommonPanelService,
-    private mqttSvc: MqttService
+    private mqttSvc: MqttService,
+    private sanitizer: DomSanitizer,
     ) { }
 
   ngOnInit(): void {
-    this.fetchRefNum()
+    this.fetchRefNum();
+    this.droneScreaming();
+    this.safeCctvUrl=this.sanitizer.bypassSecurityTrustResourceUrl(this.cctvStream)
   }
 
   ngOnDestroy(): void {
@@ -95,6 +105,7 @@ export class FpvPanelComponent implements OnInit {
         this.pubTopic = 'drone/'+this.drone_ref_num+'/rpc/send';
         this.commandTopic = 'drone/'+this.drone_ref_num+'/rpc/recv';
         this.mqttGetDroneStatus();
+        this.mqttCommandRsp();
       }
     }
   }
@@ -109,7 +120,7 @@ export class FpvPanelComponent implements OnInit {
     })
   }
 
-  //转换摄像模式开关
+  //转换镜头模式开关
   modeSwitch(mode:any) {
     this.modeCommand['id'] = "ids-" + new Date().getTime();
     this.modeCommand['params']['Display_Mode'] = mode;
@@ -148,6 +159,199 @@ export class FpvPanelComponent implements OnInit {
       let item = JSON.parse(message.payload.toString());
       console.log(item)
     })
+  }
+
+  // Janus Drone 直播串流
+  droneScreaming() {
+    var server = environment.janusServer +'/janus';
+    // var server = 'https://ids.hdcircles.tech' + ':8089'+ '/janus'
+    // var server = 'wss://janus.conf.meetecho.com/ws';
+    var streaming:any = null;
+    var opaqueId = "streamingtest-"+Janus.randomString(12);
+    var bitrateTimer:any = null;
+    var spinner:any = null;
+    var simulcastStarted:boolean = false;
+    var svcStarted = false;
+    var janus:any = null;
+    var selectedStream = "1";
+    var that = this;
+    const setupDeps = () => Janus.useDefaultDependencies({ adapter });
+    
+    Janus.init({debug: "all",dependencies: setupDeps(), callback: function() {
+      // Use a button to start the demo
+        // Make sure the browser supports WebRTC
+      if(!Janus.isWebrtcSupported()) {
+        alert("No WebRTC support... ");
+        return;
+      }
+      // Create session
+      janus = new Janus(
+        {
+          server: server,
+          success: function() {
+            // Attach to Streaming plugin
+            janus.attach(
+              {
+                //绑定janus的streaming插件
+                plugin: "janus.plugin.streaming",
+                //随机值，插件的唯一ID
+                opaqueId: opaqueId,
+
+                //attch方法执行成功后回调的函数
+                success: function(pluginHandle:any) {
+                  streaming = pluginHandle;
+                  startStream();
+                },
+                //attach方法执行失败后的回调函数
+                error: function(error:any) {
+                  console.error("  -- Error attaching plugin... ", error);
+                },
+                //可以通过该函数更新ICE状态
+                iceState: function(state:any) {
+                  console.log("ICE state changed to " + state);
+                },
+                //更改WebRTC状态的回调函数
+                webrtcState: function(on:any) {
+                  console.log("Janus says our WebRTC PeerConnection is " + (on ? "up" : "down") + " now");
+                },
+                //收到事件消息的回调函数
+                onmessage: function(msg:any, jsep:any) {
+                  console.log(" ::: Got a message :::", msg);
+                  var result = msg["result"];
+                  console.log('onmessag:',msg)
+                  if(result) {
+                    if(result["status"]) {
+                      var status = result["status"];
+                      console.warn('Status:',status);
+                      
+                    } else if(msg["streaming"] === "event") {
+                      // Is simulcast in place?
+                      var substream = result["substream"];
+                      var temporal = result["temporal"];
+                      if((substream !== null && substream !== undefined) || (temporal !== null && temporal !== undefined)) {
+                        if(!simulcastStarted) {
+                          simulcastStarted = true;
+                          // addSimulcastButtons(remoteFeed.videoCodec === "vp8");
+                        }
+                        // We just received notice that there's been a switch, update the buttons
+                        // updateSimulcastButtons(substream, temporal);
+                      }
+                      // Is VP9/SVC in place?
+                      var spatial = result["spatial_layer"];
+                      temporal = result["temporal_layer"];
+                      if((spatial !== null && spatial !== undefined) || (temporal !== null && temporal !== undefined)) {
+                        if(!svcStarted) {
+                          svcStarted = true;
+                          // addSvcButtons();
+                        }
+                        // We just received notice that there's been a switch, update the buttons
+                        // updateSvcButtons(spatial, temporal);
+                      }
+                    }
+                  } else if(msg["error"]) {
+                    console.error(msg["error"]);
+                    stopStream();
+                    return;
+                  }
+                  if(jsep) {
+                    console.log("Handling SDP as well...", jsep);
+                    var stereo = (jsep.sdp.indexOf("stereo=1") !== -1);
+                    // Offer from the plugin, let's answer
+                    streaming.createAnswer(
+                      {
+                        jsep: jsep,
+                        // We want recvonly audio/video and, if negotiated, datachannels
+                        media: { audioSend: false, videoSend: false, data: true },
+                        customizeSdp: function(jsep:any) {
+                          if(stereo && jsep.sdp.indexOf("stereo=1") == -1) {
+                            // Make sure that our offer contains stereo too
+                            jsep.sdp = jsep.sdp.replace("useinbandfec=1", "useinbandfec=1;stereo=1");
+                          }
+                        },
+                        success: function(jsep:any) {
+                          console.log("Got SDP!", jsep);
+                          var body = { request: "start" };
+                          streaming.send({ message: body, jsep: jsep });
+                        },
+                        error: function(error:any) {
+                          console.error("WebRTC error:", error);
+                        }
+                      });
+                  }
+                },
+                //收到远端流时的回调函数
+                onremotestream: function(stream:any) {
+                  console.log(" ::: Got a remote stream :::", stream);
+                  // let video = '<video autoplay muted controls id="remotevideo" width="100%" poster="../../../assets/ids_place_holder.jpg">'+'</video>'
+                  // let scream:any = document.getElementById('scream');
+                  // scream.innerHTML = video
+                  let videoElement = document.getElementById('drone')
+                  attachMediaStream(videoElement, stream);
+                  var videoTracks = stream.getVideoTracks();
+                  if(!videoTracks || videoTracks.length === 0)
+                  return;
+                },
+
+                ondataopen: function(data:any) {
+                  console.log("The DataChannel is available!");
+                },
+
+                ondata: function(data:any) {
+                  // Janus.debug("We got data from the DataChannel!", data);
+                },
+                //销毁时的回调函数
+                oncleanup: function() {
+                  console.log(" ::: Got a cleanup notification :::");
+                  stopStream()
+                }
+              });
+          },
+          error: function(error:any) {
+            console.error(error);
+              // window.location.reload();
+          },
+          destroyed: function() {
+            // window.location.reload();
+          }
+        });
+    }});
+
+
+  function getStreamInfo() {
+    // Send a request for more info on the mountpoint we subscribed to
+    var body = { request: "info", id: parseInt(selectedStream) || selectedStream };
+    streaming.send({ message: body, success: function(result:any) {
+    }});
+  }
+  
+  function startStream() {
+    console.log("Selected video id #" + selectedStream);
+    var body = { request: "watch", id: parseInt(selectedStream) || selectedStream};
+    streaming.send({ message: body });
+ 
+
+    getStreamInfo();
+  }
+  
+  function stopStream() {
+    var body = { request: "stop" };
+    streaming.send({ message: body });
+    streaming.hangup();
+  
+  }
+  // 捕捉视频流到video元素上
+  function attachMediaStream(element:any, stream:any) {
+    try {
+      element.srcObject = stream;
+      that.showToolbar = true;
+    } catch (e) {
+      try {
+        element.src = URL.createObjectURL(stream);
+      } catch (e) {
+        console.error("Error attaching stream to element");
+      }
+    }
+  };
   }
 
 }
