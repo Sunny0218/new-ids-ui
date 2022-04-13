762f41c5bc21930fabef501738310ba7fecfcda2
Sunny Wong <254450600@qq.com>
2022-04-13 18:54:04 +0800

    update ui

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute,NavigationEnd, Router } from '@angular/router'
+import { PlatformLocation } from '@angular/common'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-project',
@@ -8,13 +10,38 @@ import { ActivatedRoute } from '@angular/router'
 })
 export class ProjectComponent implements OnInit {
   shoeName:string
-  constructor(private route: ActivatedRoute) { }
+  routerPath:any;
+  private subscription = new Subscription();
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+    ) {
+      //同时具有监听的功能
+      this.subscription = this.router.events.subscribe((data) => {
+        //data返回一堆路由事件，所有得筛选自己需要的，这里选择路由导航结束之后
+        if(data instanceof NavigationEnd){
+          //筛选url最后一个斜线后的路径
+          let str = data.url;
+          let index = str.lastIndexOf("\/");
+          this.routerPath = str.substring(index + 1, str.length);
+        }
+      })
+    }
 
   ngOnInit(): void {
     this.route.params.subscribe((data) => {
       this.shoeName = data.pid
     })
     localStorage.setItem('id', this.shoeName);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  getMediaRoute() {
+    return this.routerPath === 'mission';
   }
 
 }
--- src/app/pages/personnel-management/personnel-management.component.ts
+++ src/app/pages/personnel-management/personnel-management.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {SelectionModel} from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-personnel-management',
@@ -7,9 +10,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonnelManagementComponent implements OnInit {
 
+  ELEMENT_DATA = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+
+  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  selection = new SelectionModel(true, []);
+  
+
   constructor() { }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+    /**
+   * Whether the number of selected elements matches the total number of rows.
+   *
+   * 所选元素的数量是否与总行数匹配。
+   *
+   */
+     isAllSelected() {
+      const numSelected = this.selection.selected.length;
+      const numRows = this.dataSource.data.length;
+      return numSelected === numRows;
+    }
+  
+    /**
+     * Selects all rows if they are not all selected; otherwise clear selection.
+     *
+     * 如果没有全部选中，则选择所有行；否则明确选择。
+     *
+     */
+    masterToggle() {
+      if (this.isAllSelected()) {
+        this.selection.clear();
+        return;
+      }
+  
+      this.selection.select(...this.dataSource.data);
+    }
+  
+    /**
+     * The label for the checkbox on the passed row
+     *
+     * 传递行上复选框的标签
+     *
+     */
+    checkboxLabel(row?): string {
+      if (!row) {
+        return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+      }
+      return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    }
+
 
 }
